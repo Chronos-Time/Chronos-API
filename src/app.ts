f2d9cb964a5c07745692a44eb9d037da64d17ee7
brownmanny26 <brownmanny26@gmail.com>
2024-05-18 02:52:29 +0000

    develop: made app change

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(cors(
     {
         origin: [
             'http://localhost:3000',
-            process.env.BUSINESS_WEBSITE
+            process.env.BUSINESS_WEBSITE!
         ],
         credentials: true,
         // methods: "GET, POST, PUT, DELETE, OPTIONS",
